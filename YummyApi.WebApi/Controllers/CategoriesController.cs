8f5b9bd74a5638dd0dbdc431ebac972e50da1888
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-10 22:29:44 +0300

    Completing Category API Transactions + CRUD Process Added [Get,Put,Post ] Process Added

--- YummyApi.WebApi/Controllers/CategoriesController.cs
+++ YummyApi.WebApi/Controllers/CategoriesController.cs
@@ -15,6 +15,12 @@ namespace YummyApi.WebApi.Controllers
         {
             _context = context;
         }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values=_context.Categories.ToList(); //tüm kategorileri listele
+            return Ok(values); //200 kodu döner ve Tüm Kategorileri Listeler
+        }
 
         [HttpPost]
         public IActionResult CreateCategory(Category category)
@@ -22,6 +28,35 @@ namespace YummyApi.WebApi.Controllers
             _context.Categories.Add(category);
             _context.SaveChanges(); //değişiklikleri kaydet
             return Ok("Kategori Ekleme İşlemi Başarılı.."); //200 kodu döner kategori ekleme işlemi başarılı
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound("Kategori Bulunamadı.."); //404 kodu döner kategori bulunamadı
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges(); //değişiklikleri kaydet
+            return Ok("Kategori Silme İşlemi Başarılı.."); //200 kodu döner kategori silme işlemi başarılı
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound("Kategori Bulunamadı.."); //404 kodu döner kategori bulunamadı
+            }
+            return Ok(category); //200 kodu döner ve kategori bilgilerini getirir
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges(); //değişiklikleri kaydet
+            return Ok("Kategori Güncelleme İşlemi Başarılı.."); //200 kodu döner kategori güncelleme işlemi başarılı
         }
     }
 }
