eb02a30598f950bdc7eee2398680ccdc457c50be
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-22 12:49:32 +0300

    Manuel Mapping Process With DTOs Added

---
+++ YummyApi.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YummyApi.WebApi.Context;
+using YummyApi.WebApi.DTOs.ContactDTOs;
+using YummyApi.WebApi.Entities;
+
+namespace YummyApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        Contact contact = new Contact();
+
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values =_context.Contacts.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDTO createContactDTO)
+        {
+            contact.ContactEmail = createContactDTO.ContactEmail;
+            contact.ContactAddress = createContactDTO.ContactAddress;
+            contact.ContactMapLocation = createContactDTO.ContactMapLocation;
+            contact.ContactOpenHours = createContactDTO.ContactOpenHours;
+            contact.ContactPhone = createContactDTO.ContactPhone;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme İşlemi Başarılı...");
+        }
+    }
+}
