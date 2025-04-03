6e1c49c07fe166204be3f7cb8830a01ddffb7ac0
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-03 15:09:10 +0300

    Message DTOs Added

---
+++ YummyApi.WebApi/DTOs/MessageDTO/GetByIDMessageDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace YummyApi.WebApi.DTOs.MessageDTO
+{
+    public class GetByIDMessageDTO
+    {
+        public int MessageID { get; set; } //Mesaj ID
+        public string MessageNameSurname { get; set; } //Mesaj Ad Soyad
+        public string MessageEmail { get; set; } //Mesaj Email
+        public string MessageSubject { get; set; } //Mesaj Konu
+        public string MessageDetail { get; set; } //Mesaj Detay
+        public DateTime SendDate { get; set; } //Mesaj Tarihi
+        public bool MessageIsRead { get; set; } //Mesaj Okundu mu 
+    }
+}
