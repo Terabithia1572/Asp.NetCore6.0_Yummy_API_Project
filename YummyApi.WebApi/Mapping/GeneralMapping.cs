fc1629b10518d53e7937b2cee268d6d9df1e115f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-03 15:09:18 +0300

    General Mapping ve Auto Mapper Added

---
+++ YummyApi.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using YummyApi.WebApi.DTOs.FeatureDTOs;
+using YummyApi.WebApi.DTOs.MessageDTO;
+using YummyApi.WebApi.Entities;
+
+namespace YummyApi.WebApi.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDTO>().ReverseMap();
+            //        AutoMapper'deki ReverseMap() metodu, iki yönlü dönüşüm sağlar. Yani, bir nesneyi diğerine dönüştürdüğünüz gibi, tersini de otomatik olarak yapmanıza olanak tanır.
+            //        Özetle:
+            // Tek satırla çift yönlü mapleme sağlar.
+            // Kod tekrarını azaltır.
+            // Kolay kullanım sunar.
+            CreateMap<Feature, CreateFeatureDTO>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDTO>().ReverseMap();
+            CreateMap<Feature, GetByIDFeatureDTO>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDTO>().ReverseMap();
+            CreateMap<Message, CreateMessageDTO>().ReverseMap();
+            CreateMap<Message, UpdateMessageDTO>().ReverseMap();
+            CreateMap<Message, GetByIDMessageDTO>().ReverseMap();
+        }
+    }
+}
