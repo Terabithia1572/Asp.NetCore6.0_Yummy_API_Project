516309cd6472a3daa78c0b4dfe7aeeb55dcf967f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-08 20:47:52 +0300

    Api Consume Backend Process Added

---
+++ YummyApi.WebUI/DTOs/ServiceDTOs/ResultServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace YummyApi.WebUI.DTOs.ServiceDTOs
+{
+    public class ResultServiceDTO
+    {
+        public int ServiceID { get; set; } //Servis ID
+        public string ServiceTitle { get; set; } //Servis Başlık
+        public string ServiceDescription { get; set; } //Servis Açıklama
+        public string ServiceIconURL { get; set; } //Servis İkon URL
+    }
+}
