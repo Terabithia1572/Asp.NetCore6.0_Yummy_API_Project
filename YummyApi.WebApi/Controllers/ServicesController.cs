65562a6107fc083d5b5fc4d62413b51c9862dbfe
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-08 21:04:46 +0300

    Listing Service Data with API Consume Process Added

---
+++ YummyApi.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YummyApi.WebApi.Context;
+using YummyApi.WebApi.Entities;
+
+namespace YummyApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ServicesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ServiceList()
+        {
+            var values = _context.Services.ToList(); //tüm hizmetleri listele
+            return Ok(values); //200 kodu döner ve Tüm Servisleri Listeler
+        }
+
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges(); //değişiklikleri kaydet
+            return Ok("Servis Ekleme İşlemi Başarılı.."); //200 kodu döner hizmet ekleme işlemi başarılı
+        }
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null)
+            {
+                return NotFound("Servis Bulunamadı.."); //404 kodu döner hizmet bulunamadı
+            }
+            _context.Services.Remove(service);
+            _context.SaveChanges(); //değişiklikleri kaydet
+            return Ok("Servis Silme İşlemi Başarılı.."); //200 kodu döner hizmet silme işlemi başarılı
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetService(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null)
+            {
+                return NotFound("Servis Bulunamadı.."); //404 kodu döner hizmet bulunamadı
+            }
+            return Ok(service); //200 kodu döner ve hizmet bilgilerini getirir
+        }
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges(); //değişiklikleri kaydet
+            return Ok("Servis Güncelleme İşlemi Başarılı.."); //200 kodu döner hizmet güncelleme işlemi başarılı
+        }
+    }
+}
--- YummyApi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ YummyApi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -16,7 +16,7 @@ namespace YummyApi.WebUI.ViewComponents
         public async Task< IViewComponentResult> InvokeAsync()
         {
             var client=_httpClientFactory.CreateClient();
-            var responseMessage= await client.GetAsync("https://localhost:7292/api/Services/");
+            var responseMessage= await client.GetAsync("https://localhost:44368/api/Services");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
