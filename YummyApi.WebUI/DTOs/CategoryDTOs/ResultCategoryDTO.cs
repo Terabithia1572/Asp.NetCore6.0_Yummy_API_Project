90b1ebb33d5f5c03f5c7cfa65ed953a88a2e7bbd
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-23 20:49:34 +0300

    Consume Process in the UI Section of Categories Added

---
+++ YummyApi.WebUI/DTOs/CategoryDTOs/ResultCategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace YummyApi.WebUI.DTOs.CategoryDTOs
+{
+    public class ResultCategoryDTO
+    {
+        public int CategoryID { get; set; } //Kategori ID
+        public string CategoryName { get; set; } //Kategori Adı
+     
+    }
+}
---
+++ YummyApi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuCategoryComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyApi.WebUI.DTOs.CategoryDTOs;
+using YummyApi.WebUI.DTOs.ServiceDTOs;
+
+namespace YummyApi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuCategoryComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44368/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
