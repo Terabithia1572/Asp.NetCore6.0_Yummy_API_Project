a05765e5e772cb9980304afba56478bd0ceb3dbe
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-29 23:53:11 +0300

    Menu Product View Component Process Added

---
+++ YummyApi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentViewPartial.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyApi.WebUI.DTOs.ProductDTOs;
+
+namespace YummyApi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductComponentViewPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultMenuProductComponentViewPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44368/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
