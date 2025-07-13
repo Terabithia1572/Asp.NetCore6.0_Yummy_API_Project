b34526e14ca8537d410182a8a8a40343e3b2cb1d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-13 18:18:45 +0300

    Chef API Process Added

---
+++ YummyApi.WebUI/DTOs/ChefDTOs/ResultChefDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace YummyApi.WebUI.DTOs.ChefDTOs
+{
+    public class ResultChefDTO
+    {
+        public int ChefID { get; set; } //Şef ID
+        public string ChefNameSurname { get; set; } // Şef Adı Soyadı
+        public string ChefTitle { get; set; } //Şef Başlık
+        public string ChefDescription { get; set; } //Şef Açıklama
+        public string ChefImageURL { get; set; } //Şef Resmi URL
+    }
+}
