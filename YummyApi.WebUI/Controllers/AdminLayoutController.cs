691bc5ebb42a8b1e31682b45715698ad07d94ccf
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-13 18:57:34 +0300

    admin Layout Theme Editing + Updated

---
+++ YummyApi.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YummyApi.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
