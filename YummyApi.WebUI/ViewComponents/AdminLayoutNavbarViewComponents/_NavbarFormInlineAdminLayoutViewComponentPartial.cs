cbdfbddc658c9ae4ab059d32371a6c2dc8458d46
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-14 20:44:03 +0300

    32# Edits in Admin Layout Process Added

---
+++ YummyApi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarFormInlineAdminLayoutViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YummyApi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarFormInlineAdminLayoutViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
