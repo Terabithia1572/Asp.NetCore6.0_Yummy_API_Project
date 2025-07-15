de7776654e40e2bd18f79c0de50082d16a2cfbdc
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-15 16:13:44 +0300

    38# Category Process in the Admin Panel

--- YummyApi.WebUI/Controllers/CategoryController.cs
+++ YummyApi.WebUI/Controllers/CategoryController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyApi.WebUI.DTOs.CategoryDTOs;
 
 namespace YummyApi.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult CategoryList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44368/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateCategory()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDTO createCategoryDTO)
+        {
+            var client = _httpClientFactory.CreateClient(); // Bu satır, denetleyiciye enjekte edilen fabrikayı kullanarak bir HTTP istemci örneği oluşturur.
+            var jsonData = JsonConvert.SerializeObject(createCategoryDTO); // JSON verisine dönüştürme işlemi yapar.
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisini UTF-8 kodlamasıyla ve "application/json" içeriğiyle bir StringContent nesnesine sarar.
+            var responseMessage = await client.PostAsync("https://localhost:44368/api/Categories", stringContent); // HTTP POST isteği gönderir ve yanıtı alır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer yanıt başarılıysa (HTTP durum kodu 2xx ise)
+            {
+                return RedirectToAction("CategoryList"); // Kategori listesini görüntülemek için CategoryList eylemine yönlendirir.
+            }
+            return View(); // Eğer yanıt başarısızsa, aynı görünümü tekrar gösterir.
         }
     }
 }
---
+++ YummyApi.WebUI/DTOs/CategoryDTOs/CreateCategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace YummyApi.WebUI.DTOs.CategoryDTOs
+{
+    public class CreateCategoryDTO
+    {
+        
+        public string CategoryName { get; set; } //Kategori Adı
+    }
+}
