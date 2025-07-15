1470fa4c3b4a5a5ef4ffc2889e4749d31b92f4a2
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-15 14:54:11 +0300

    35# Notification List in Navbar Process Added

---
+++ YummyApi.WebUI/DTOs/NotificationDTOs/ResultNotificationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace YummyApi.WebUI.DTOs.NotificationDTOs
+{
+    public class ResultNotificationDTO
+    {
+        public int NotificationID { get; set; } //Bildirim ID'sini tutuyoruz burada
+        public string NotificationDescription { get; set; } //Bildirim açıklamasını tutuyoruz
+        public string NotificationIconURL { get; set; } //Bildirim simge URL'sini tutuyoruz
+        public DateTime NotificationDate { get; set; } //Bildirim tarihini tutuyoruz
+        public bool NotificationIsRead { get; set; } //Bildirim okunmuş mu kontrol ediyoruz
+    }
+}
---
+++ YummyApi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarNotificationListAdminLayoutComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyApi.WebUI.DTOs.NotificationDTOs;
+
+namespace YummyApi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarNotificationListAdminLayoutComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _NavbarNotificationListAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseNotification = await client.GetAsync("https://localhost:44368/api/Notifications");
+            if (responseNotification.IsSuccessStatusCode)
+            {
+                var jsonData = await responseNotification.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
