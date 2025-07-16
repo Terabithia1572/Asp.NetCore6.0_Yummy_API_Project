82a19460a05237704a5779fc072a5f4d6b80ac73
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-07-16 20:25:40 +0300

    41# Product UI Process in the Admin Panel

---
+++ YummyApi.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using YummyApi.WebUI.DTOs.ProductDTOs;
+
+namespace YummyApi.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44368/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDTO createProductDTO)
+        {
+            var client = _httpClientFactory.CreateClient(); // Bu satır, denetleyiciye enjekte edilen fabrikayı kullanarak bir HTTP istemci örneği oluşturur.
+            var jsonData = JsonConvert.SerializeObject(createProductDTO); // JSON verisine dönüştürme işlemi yapar.
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json"); // JSON verisini UTF-8 kodlamasıyla ve "application/json" içeriğiyle bir StringContent nesnesine sarar.
+            var responseMessage = await client.PostAsync("https://localhost:44368/api/Products", stringContent); // HTTP POST isteği gönderir ve yanıtı alır.
+            if (responseMessage.IsSuccessStatusCode) // Eğer yanıt başarılıysa (HTTP durum kodu 2xx ise)
+            {
+                return RedirectToAction("ProductList"); // Kategori listesini görüntülemek için ProductList eylemine yönlendirir.
+            }
+            return View(); // Eğer yanıt başarısızsa, aynı görünümü tekrar gösterir.
+        }
+
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:44368/api/Products?id=" + id);
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44368/api/Products/GetProduct?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetProductByIDDTO>(jsonData);
+            return View(value);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDTO updateProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PutAsync("https://localhost:44368/api/Products/", stringContent);
+            return RedirectToAction("ProductList");
+        }
+    }
+}
---
+++ YummyApi.WebUI/DTOs/ProductDTOs/GetProductByIDDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace YummyApi.WebUI.DTOs.ProductDTOs
+{
+    public class GetProductByIDDTO
+    {
+        public int ProductID { get; set; } //Ürün ID
+        public string ProductName { get; set; } //Ürün Adı
+        public string ProductDescription { get; set; } //Ürün Açıklaması
+        public decimal ProductPrice { get; set; } // Ürün Fiyatı
+        public string ProductImageURL { get; set; } //Ürün Resmi URL
+
+        public int CategoryID { get; set; } //? Boş geçilebilir anlamı katıyor.. Kategori ile Ürünleri İlişkili hale getirdik
+    }
+}
