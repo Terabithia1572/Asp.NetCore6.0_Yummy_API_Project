9fc4f3293ec32f33f00af16db90536e806b5a93c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-09-13 15:24:19 +0300

    69# Dashboard Process Added

---
+++ YummyApi.WebUI/ViewComponents/DashboardViewComponents/_DashboardWidgetsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YummyApi.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardWidgetsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
